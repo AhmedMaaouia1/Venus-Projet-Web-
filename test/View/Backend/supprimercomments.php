2b094a48ee466d163430fd27d93471edff5c7fcb
iskanderbahroun <iskander.bahroun@esprit.tn>
2022-05-08 08:34:31 +0100

    integration du forum

--- test/View/Backend/supprimercomments.php
+++
@@ -1,7 +0,0 @@
-<?php
-    require 'C:/xampp/htdocs/ESSAI 1 INTEGRATION/test/Controller/commentsC.php';
-
-    $commentsC = new commentsC();
-    $commentsC->supprimercomments($_GET['id']);
-    header('Location:liste_commentaire.php');
-?>
--- test/View/Backend/supprimertopic.php
+++
@@ -1,7 +0,0 @@
-<?php
-    require 'C:/xampp/htdocs/ESSAI 1 INTEGRATION/test/Controller/topicC.php';
-
-    $topicC = new topicC();
-    $topicC->supprimertopic($_GET['id']);
-    header('Location:liste_forum.php');
-?>
